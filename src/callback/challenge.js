08d3d68ccda8fc735c925ba4f8ad54ce7eabd495
Sunjing Lama <luislama@espol.edu.ec>
2020-12-20 11:20:17 -0500

    Upd: challenge done

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,4 +1,6 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/';
+
 
 function fetchData(url_api, callback) {
     let xhttp = new XMLHttpRequest();
@@ -15,3 +17,18 @@ function fetchData(url_api, callback) {
     }
     xhttp.send();
 }
+
+
+fetchData(API, function (error1, data1) {
+    if (error1) return console.error(error1);
+    console.log("numero de personajes: ", data1.info.count);
+    console.log("ID de 1er personaje: ", data1.results[0].id);
+    fetchData(API + data1.results[0].id, function(error2, data2) {
+        if (error2) return console.error(error2);
+        console.log("nombre 1er personaje: ", data2.name);
+        fetchData(data2.origin.url, function(error3, data3) {
+            if (error3) return console.error(error3);
+            console.log("nombre dimension 1er personaje: ", data3.dimension);
+        });
+    });
+});
